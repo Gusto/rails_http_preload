3a3de3ada9da61fd1d7de5353c0a7c18dd656853
Nate Berkopec <nate.berkopec@gusto.com>
2023-03-22 11:02:44 +0900

    Fixup from feedback

--- lib/rails_http_preload/middleware.rb
+++ lib/rails_http_preload/middleware.rb
@@ -13,41 +13,44 @@ module RailsHttpPreload
     def call(env)
       response = @app.call(env)
 
-      header = PreconnectHeader.new(env, response)
-      response[1]["Link"] = header.to_s if header.required?
+      response[1]["Link"] = preconnect_header if required?(response, ActionDispatch::Request.new(env))
 
       response
     end
 
-    class PreconnectHeader
-      attr_reader :request, :response
+    # TODO: Should we just add it to every response anyway and let the browser
+    # figure it out?
+    def required?(response, request)
+      html?(response) &&
+        !already_connected?(request)
+    end
 
-      include ActionView::Helpers::AssetUrlHelper
+    # Adding this header to, for example, a JSON response would be pointless
+    # TODO: remove from Turbo frame responses too
+    def html?(response)
+      response[1]["Content-Type"].match?("html")
+    end
 
-      def initialize(env, response)
-        @request = ActionDispatch::Request.new(env)
-        @response = response
-      end
+    # If the asset host is equal to the request domain, no need to add.
+    def already_connected?(request)
+      protocol = request.protocol.gsub("://", "")
+      ActionController::Base.helpers.compute_asset_host("", host: config.asset_host) ==
+        ActionController::Base.helpers.compute_asset_host("", { protocol: protocol, host: request.domain })
+    end
 
-      def required?
-        response[1]["Content-Type"].match?("html") &&
-          compute_asset_host(config.asset_host) != compute_asset_host("", host: request.domain)
-      end
+    def preconnect_header
+      [
+        ActionController::Base.helpers.compute_asset_host("", host: config.asset_host),
+        *config.additional_urls
+      ].compact.map { |url| create_link_header(url) }.join(", ")
+    end
 
-      def to_s
-        [
-          compute_asset_host(config.asset_host),
-          *config.additional_urls
-        ].compact.map { |url| create_link_header(url) }.join(", ")
-      end
+    def create_link_header(url)
+      "<#{url}>; rel=preconnect"
+    end
 
-      def create_link_header(url)
-        "<#{url}>; rel=preconnect"
-      end
-
-      def config
-        RailsHttpPreload.config
-      end
+    def config
+      RailsHttpPreload.config
     end
   end
 end
